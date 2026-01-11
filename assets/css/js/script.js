eab0bb9e37a6ec0e25ccca361fe94f218874da9f
Carmelo0603 <carmeloprovenzani1@gmail.com>
2026-01-11 18:05:05 +0100

    js e sticky header finito

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,5 +1,26 @@
+//ANIMAZIONE DELLE LETTERE//
 const getLetters = document.querySelectorAll("#containerSvg path");
-console.log(getLetters);
-function twinkle() {
-  getLetters;
-}
+
+setInterval(() => {
+  getLetters.forEach((letter) => {
+    let casualNumb = Math.random();
+
+    if (casualNumb > 0.5) {
+      letter.classList.add("m-on");
+      letter.classList.remove("m-off");
+    } else {
+      letter.classList.add("m-off");
+      letter.classList.remove("m-on");
+    }
+  });
+}, 900);
+
+//SCROLL HEADER//
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 350) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
